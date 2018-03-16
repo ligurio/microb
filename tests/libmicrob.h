ec5b7de34f9f480f37231b17f62d398ca02c84f8
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-03-16 18:58:44 +0300

    Move to libmicrob.h

---
+++ tests/libmicrob.h
@@ -0,0 +1,16 @@
+/*
+	microb - The micro performance test framework for C99
+	
+	Author - Sergey Bronnikov (https://github.com/ligurio/microb)
+ 
+	This file provides both the interface and the implementation.
+
+	USAGE
+	-----
+	See README.md for details
+
+	LICENSE
+	-------
+	TODO
+ */
+
